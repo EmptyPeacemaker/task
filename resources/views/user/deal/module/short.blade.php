e702a8f3d6370bf7ca7430d164a31581a5166699
EmptyPeacemaker <2003kirillavdeev@gmail.com>
2021-06-19 23:21:14 +0500

    first commit

---
+++ resources/views/user/deal/module/short.blade.php
@@ -0,0 +1,32 @@
+<img src="{{$deal->img}}" class="rounded float-end" alt="...">
+<h3>{{$deal->title}}</h3>
+<div>{{$deal->description}}</div>
+<hr>
+<div class="row justify-content-between">
+    <div class="col">Время на выполнение:</div>
+    <div class="col">{{\Carbon\CarbonInterval::seconds($deal->times)->cascade()->locale('ru')->forHumans()}}</div>
+</div>
+<div class="row justify-content-between">
+    <div class="col">Стоимость:</div>
+    <div class="col">{{$deal->price}} ₽</div>
+</div>
+<hr>
+<div class="row justify-content-between">
+    <div class="col">Статус:</div>
+    <div class="col">{{$deal->getStatus->title}}</div>
+</div>
+
+@switch(\Illuminate\Support\Facades\Auth::user()->role)
+    @case(1)
+    <div class="row justify-content-between">
+        <div class="col">Автор:</div>
+        <div class="col">{{$deal->getAuthor->name}}</div>
+    </div>
+    @break
+    @case(2)
+    <div class="row justify-content-between">
+        <div class="col">Исполнитель:</div>
+        <div class="col">{{$deal->getExecutor->name?? "Исполнитель не назначен"}}</div>
+    </div>
+    @break
+@endswitch
---
+++ resources/views/user/deal/show.blade.php
@@ -0,0 +1,53 @@
+@extends('layout.index')
+@section('title')
+    Задача
+@endsection
+@section('content')
+    <div class="card text-center">
+        <div class="card-header">
+            <nav>
+                <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                    <div class="btn nav-link active" id="nav-short-tab" data-bs-toggle="tab" data-bs-target="#nav-short"
+                         role="tab" aria-controls="nav-short">Кратко
+                    </div>
+                    <div class="btn nav-link" id="nav-description-tab" data-bs-toggle="tab"
+                         data-bs-target="#nav-description"
+                         role="tab" aria-controls="nav-description">Подробное описание
+                    </div>
+                    <div class="btn nav-link" id="nav-edit-tab" data-bs-toggle="tab"
+                         data-bs-target="#nav-edit"
+                         role="tab" aria-controls="nav-edit">Редактирование
+                    </div>
+                </div>
+            </nav>
+        </div>
+        <div class="card-body">
+            <div class="tab-content text-start" id="nav-tabContent">
+                <div class="tab-pane fade show active" id="nav-short" role="tabpanel" aria-labelledby="nav-short-tab">
+                    @include('user.deal.module.short')
+                </div>
+                <div class="tab-pane fade" id="nav-description" role="tabpanel" aria-labelledby="nav-description-tab">
+                    @include('user.deal.module.description')
+                </div>
+                <div class="tab-pane fade" id="nav-edit" role="tabpanel" aria-labelledby="nav-edit-tab">
+                    @include('user.deal.module.edit')
+                </div>
+            </div>
+        </div>
+    </div>
+
+{{--    <div class="card text-start btn m-1 p-0">--}}
+{{--        <div class="card-"></div>--}}
+{{--        <div style="height: 210px;"--}}
+{{--             class="overflow-hidden d-flex justify-content-center align-items-center">--}}
+{{--            <img src="{{$deal->img}}" class="card-img-top">--}}
+{{--        </div>--}}
+{{--        <div class="card-body">--}}
+{{--            <h5 class="card-title">{{$deal->title}}</h5>--}}
+{{--            <p class="card-text">{{$deal->description}}</p>--}}
+{{--        </div>--}}
+{{--        <div class="card-footer" style="white-space: nowrap">--}}
+{{--            {{$deal->getStatus->title}}--}}
+{{--        </div>--}}
+{{--    </div>--}}
+@endsection
